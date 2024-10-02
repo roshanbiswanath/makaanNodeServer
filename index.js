9e089c3258f148f8a732b5f751be2fc041c5d489
roshanbiswanath <roshanbiswanathpatra@gmail.com>
2024-10-02 18:14:41 +0530

    Scene Change and Teleportation

--- index.js
+++ index.js
@@ -5,22 +5,24 @@ const { connect } = require('http2');
 const server = http.createServer(app);
 const fs = require('fs');
 require('dotenv').config()
+var cors = require('cors')
+
 
 const { Server } = require("socket.io");
-const io = new Server(server);
+const io = new Server(server,{
+    cors: {
+        origin: "*",
+        methods: ["GET", "POST"]
+    }
+});
+app.use(cors())
 
 const { initializeApp, cert } = require("firebase-admin/app");
 
-console.log(process.env.FIREBASE_SECRET);
-// Write the Firebase Console service account key JSON to a file
 fs.writeFileSync("./vr-realestate-demo-firebase-adminsdk-q9gbz-ef6209c3d7.json", process.env.FIREBASE_SECRET, "utf-8");
 
-
-
-// Fetch the service account key JSON file contents
 var serviceAccount = require("./vr-realestate-demo-firebase-adminsdk-q9gbz-ef6209c3d7.json");
 
-// Initialize the app with a custom auth variable, limiting the server's access
 initializeApp({
     credential: cert(serviceAccount)
 });
@@ -37,7 +39,7 @@ async function getDb() {
     });
 }
 
-getDb().catch(console.error);
+// getDb().catch(console.error);
 
 
 headsetConnections = {}
@@ -46,20 +48,20 @@ connections = {}
 
 io.on('connection', (socket) => {
     console.log('a user connected');
-    console.log(connections);
+    // console.log(connections);
     socket.on('disconnect', () => {
         console.log('user disconnected');
         headsetConnections = Object.fromEntries(Object.entries(headsetConnections).filter(([key, value]) => value.socketId !== socket.id));
-        console.log(headsetConnections);
+        // console.log(headsetConnections);
     });
     socket.on('headsetStatusUpdate', (msg) => {
-        console.log('message: ' + msg);
+        // console.log('message: ' + msg);
         connections[msg] = {
             socketId: socket.id,
             status: "Online",
             connection: "Open"
         };
-        console.log(connections)
+        // console.log(connections)
         io.emit('deviceStatus', msg);
     })
     // socket.on('controllerConnection', (ownerId) => {
@@ -69,15 +71,51 @@ io.on('connection', (socket) => {
     //         status: "Online"
     //     }
     // });
+
+    socket.on('sceneChangeCommand', (command) => { 
+        console.log('Scene Change: ' + command.sceneID);
+        if(!(command.deviceID in connections)) {
+            console.log('Device not connected');
+            return;
+        }
+        let socketId = connections[command.deviceID].socketId;
+        io.to(socketId).emit('sceneChange', command.sceneID);
+        // io.emit('sceneChange', command);
+    })
+    socket.on('teleChangeCommand', (command) => { 
+        console.log('Tele Change: ' + command.teleID);
+        if(!(command.deviceID in connections)) {
+            console.log('Device not connected');
+            return;
+        }
+        let socketId = connections[command.deviceID].socketId;
+        io.to(socketId).emit('teleChange', command.teleID);
+        // io.emit('sceneChange', command);
+    })
 });
 
 app.get('/', (req, res) => {
     res.send('<h1>Hello world</h1>');
 });
 
-app.get('/headSets', (req, res) => {
-
-    res.send(headsetConnections);
+app.get('/headsets/:ownerId', async (req, res) => {
+    // console.log(req.params.ownerId);
+    // console.log()
+    const snapshot = await db.collection('devices').where('owner', '==', req.params.ownerId).get();
+    let retObj = {}
+    // for (const doc of snapshot.docs) {
+    //     console.log(doc.id, '=>', doc.data());
+    // }
+    for (const doc of snapshot.docs) {
+        retObj[doc.id] = doc.data();
+        if (doc.data().deviceID in connections) {
+            retObj[doc.id].status = connections[doc.data().deviceID].status;
+        }
+        else {
+            retObj[doc.id].status = "Offline";
+        }
+    }
+    res.send(retObj);
 });
 
 server.listen(3000, () => {
